dfeff4b406c46d0d792469bc11576e3cf232c2e3
figueiredo-uideveloper <uideveloper@figueiredo.dev>
2019-12-27 19:06:03 -0200

    Sets next function

--- js/dropslider.js
+++ js/dropslider.js
@@ -1,9 +1,47 @@
-const dropslider = document.querySelectorAll('.dropslider');
+// function move(item, callback) {
+//     item.style.transform = 'translateX(0)';
+//     callback();
+// }
+
+function next(slides, currentSlide) {
+    const i = slides.indexOf(currentSlide) + 1;
+    let nextSlide = i < slides.length ? slides[i] : slides[0];
+    currentSlide.style.transform = 'translateX(-100%)';
+    nextSlide.style.transform = 'translateX(0)';
+}
+
+const buttonNext = document.querySelector('.dropslider-controllers__next');
 const dropsliderItems = document.querySelectorAll('.dropslider__item');
+const slides = Array.from(dropsliderItems);
+
+buttonNext.addEventListener('click', () => {
+    let currentSlide = document.querySelector('.is-active');
+    console.log(currentSlide);
+    next(slides, currentSlide);
+});
 
 
-dropsliderItems.forEach(
-    item => item.getAttribute('aria-hidden') === true ? console.log('T') : console.log('F')
-);
 
-// console.log(dropsliderItems[0].getAttribute('aria-hidden'));
+// function getNext(list, current) {
+//     const i = list.indexOf(current) + 1;
+//     return i < list.length ? list[i] : list[0];
+// }
+
+
+
+// function slider() {
+//     const dropsliderItems = document.querySelectorAll('.dropslider__item');
+//     const slides = Array.from(dropsliderItems);
+
+//     showSlide(slides, slides[0]);
+// }
+
+// function showSlide(slides, slide) {
+//     slide.style.transform = 'translateX(0)';
+
+//     move(slide, () => {
+//         slide.style.transform = 'translateX(-100%)';
+//         showSlide(slides, getNext(slides, slide));
+//     });
+// }
+
