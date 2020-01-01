52a0578ad6026ad148d08b488916abc9ac1e1b51
figueiredo-uideveloper <uideveloper@figueiredo.dev>
2020-01-01 19:14:38 -0300

    Sets prev function

--- js/dropslider.js
+++ js/dropslider.js
@@ -1,11 +1,11 @@
 const tl = gsap.timeline();
+let slideTime = 1;
 
 function setInitialPosition(slide) {
     tl.set(slide, {x: "100%"});
 }
 
 function next(activeSlide, nextSlide) {
-    let slideTime = 1;
     tl.to(activeSlide, { duration: slideTime, x: "-100%" })
         .to(nextSlide, { duration: slideTime, x: "0" }, `=-${slideTime}`)
         .set(activeSlide, { x: "100%" });
@@ -13,11 +13,26 @@ function next(activeSlide, nextSlide) {
     updateActiveSlide(activeSlide, nextSlide);
 }
 
-function getNext(slides, activeSlide) {    
+function prev(activeSlide, prevSlide) {
+    tl.set(prevSlide, { x: "-100%" })
+        .to(activeSlide, { duration: slideTime, x: "100%" })
+        .to(prevSlide, { duration: slideTime, x: "0" }, `=-${slideTime}`);
+    
+    updateActiveSlide(activeSlide, prevSlide);
+}
+
+function getNext(slides, activeSlide) {
     const index = slides.indexOf(activeSlide) + 1;
     let nextSlide = index < slides.length ? slides[index] : slides[0];
 
     return nextSlide;
+}
+
+function getPrev(slides, activeSlide) {
+    const index = slides.indexOf(activeSlide) - 1;
+    let prevSlide = index < slides.length && index > 0 ? slides[index] : slides[3];
+
+    return prevSlide;
 }
 
 function updateActiveSlide(currentSlide, otherSlide) {
@@ -37,6 +52,13 @@ const buttonNext = document.querySelector('.dropslider-controllers__next');
 buttonNext.addEventListener('click', () => {
     let activeSlide = document.querySelector('.is-active');
     next(activeSlide, getNext(slides, activeSlide));
+});
+
+const buttonPrev = document.querySelector('.dropslider-controllers__prev');
+
+buttonPrev.addEventListener('click', () => {
+    let activeSlide = document.querySelector('.is-active');
+    prev(activeSlide, getPrev(slides, activeSlide));
 });
 
 let automatico = false;
